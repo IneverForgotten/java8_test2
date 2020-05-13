5732c87dfbb0650baa783609eb893d51f3ae91c2
fanlm <fanlm521@163.com>
2020-05-13 19:23:31 +0800

    project init

---
+++ src/main/java/com/fanlm/Java8TestApplication.java
@@ -0,0 +1,13 @@
+package com.fanlm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fanlm/Java8TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanlm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
