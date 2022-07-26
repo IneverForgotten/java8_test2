ba36f73b27c5ceb2db85b8daa84bb131c99b2e80
fanleimin <fanleimin@datangwealth.com>
2022-07-26 17:20:38 +0800

    测试连接

---
+++ src/main/java/com/fanlm/algorithm/TreeSort.java
@@ -0,0 +1,13 @@
+package com.fanlm.algorithm;
+
+/**
+ * @ClassName: TreeSort
+ * @Description:
+ * @Author: fanLeiMin
+ * @Date: 2022/7/26 17:20
+ */
+public class TreeSort
+{
+
+
+}
---
+++ src/main/java/com/fanlm/test/BasicsTest.java
@@ -0,0 +1,15 @@
+package com.fanlm.test;
+
+/**
+ * @ClassName: BasicsTest
+ * @Description:
+ * @Author: fanLeiMin
+ * @Date: 2022/6/10 9:57
+ */
+public class BasicsTest {
+    public static void main(String[] args) {
+        char m = 'm';
+        char f = 'f';
+        System.out.println(m^f);
+    }
+}
